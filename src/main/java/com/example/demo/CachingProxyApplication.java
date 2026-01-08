1819e4d190ae7e99c511d3703a2d4bbdcb2777cc
BryanYuen <bryanyuen5@gmail.com>
2026-01-08 13:26:26 +0800

    Implemented Dynamic Path Get

--- src/main/java/com/example/demo/CachingProxyApplication.java
+++ src/main/java/com/example/demo/CachingProxyApplication.java
@@ -1,31 +1,65 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
 
 @RestController
 public class CachingProxyApplication {
 
+    private final RestTemplate restTemplate;
+    private final String originBaseUrl;
+
+    public CachingProxyApplication(RestTemplate restTemplate,
+                                   @Value("${proxy.origin}") String originBaseUrl) {
+        this.restTemplate = restTemplate;
+        this.originBaseUrl = originBaseUrl;
+    }
+
     @GetMapping("/hello")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String getHello() {
+        return "Hello from Spring Boot";
     }
 
-    @PostMapping("/bye")
-    public String bye(){
-        return "bye";
-    }
+//    @GetMapping("/products")
+//    public ResponseEntity<String> proxyProducts(){
+//        String url = originBaseUrl + "/products";
+//
+//        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+//
+//        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+//    }
 
-    @GetMapping("/foos")
-    public String getFoos(@RequestParam String id) {
-        return "ID: " + id;
-    }
+    @GetMapping("/{*path}")
+    public ResponseEntity<String> proxyGet(@PathVariable("path") String path, @RequestParam Map<String, String> queryParams){
+        StringBuilder url = new StringBuilder(originBaseUrl);
 
-    @GetMapping("/api/employeeswithvariable/{id}")
-    public String getEmployeesByIdWithVariableName(@PathVariable("id") String employeeId) {
-        return "ID: " + employeeId;
+        if(!path.isEmpty()){
+            if(!originBaseUrl.endsWith("/") && !path.startsWith("/")){
+                url.append("/");
+            }
+            url.append(path);
+        }
+
+        if (!queryParams.isEmpty()) {
+            url.append("?");
+            url.append(
+                    queryParams.entrySet().stream()
+                            .map(e -> e.getKey() + "=" + e.getValue())
+                            .reduce((a, b) -> a + "&" + b)
+                            .orElse("")
+            );
+        }
+
+        ResponseEntity<String> response = restTemplate.getForEntity(url.toString(), String.class);
+        return ResponseEntity
+                .status(response.getStatusCode())
+                .body(response.getBody());
     }
 }
